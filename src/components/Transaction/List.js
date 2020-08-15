2812d1a797b4ef1f078309277377a511f45901ed
Ricky Li <nzrickyli@gmail.com>
2020-08-16 11:24:39 +1200

    use material ui list component

--- src/components/Transaction/List.js
+++ src/components/Transaction/List.js
@@ -1,15 +1,72 @@
 import React from 'react';
 
-const TransactionList = ({transactions}) => {
-console.log(transactions);
-    return (transactions.map((item) => (
-        <tr key={item.id}>
-          <td>{item.date}</td>
-          <td>{item.amount}</td>
-          <td>{item.category}</td>
-        </tr>
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess'
+import ListSubheader from '@material-ui/core/ListSubheader';
 
-      )));
+const TransactionList = ({ transactions }) => {
+  console.log(transactions);
+  return (transactions.map((item) => (
+    <>
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar>
+          <ImageIcon />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary={item.category} secondary={item.date} />
+      <ListSubheader>${item.amount}</ListSubheader>
+    </ListItem>
+
+<ListItem>
+<ListItemAvatar>
+  <Avatar>
+    <ImageIcon />
+  </Avatar>
+</ListItemAvatar>
+<ListItemText primary={item.category} secondary={item.date} />
+</ListItem>
+
+<ListItem>
+<ListItemAvatar>
+  <Avatar>
+    <ImageIcon />
+  </Avatar>
+</ListItemAvatar>
+<ListItemText primary={item.category} secondary={item.date} />
+</ListItem>
+
+<ListItem>
+<ListItemAvatar>
+  <Avatar>
+    <ImageIcon />
+  </Avatar>
+</ListItemAvatar>
+<ListItemText primary={item.category} secondary={item.date} />
+</ListItem>
+
+<ListItem>
+<ListItemAvatar>
+  <Avatar>
+    <ImageIcon />
+  </Avatar>
+</ListItemAvatar>
+<ListItemText primary={item.category} secondary={item.date} />
+</ListItem>
+</>
+
+    // <tr key={item.id}>
+    //   <td>{item.date}</td>
+    //   <td>{item.amount}</td>
+    //   <td>{item.category}</td>
+    // </tr>
+
+  )));
 };
 
 export default TransactionList;
